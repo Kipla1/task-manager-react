809c0806f24533507adc1e2d592227a2bffc64d9
Erykah <erkahwanga@gmail.com>
2025-04-11 00:07:08 +0300

    Enhance TaskInputForm and App styles with new features and improved aesthetics

--- src/TaskInputForm.jsx
+++ src/TaskInputForm.jsx
@@ -1,12 +1,21 @@
 function TaskInputForm() {
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log('I was clicked');
+    console.log(1 + 1);
+  };
+
   return (
     <>
       <form className='task-form'>
         <input type='text' placeholder='Title' className='input' />
         <textarea placeholder='Description' className='input textarea' />
-        <button type='submit' className='btn add-btn'>
+        <button type='submit' className='btn add-btn' onClick={handleClick}>
           Add Task
         </button>
+        {/* <button type='submit' className='btn add-btn' onClick={handleClick()}>
+          Add Task
+        </button> */}
       </form>
     </>
   );
