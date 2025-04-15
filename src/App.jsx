d225586bc33e3c3c7b01bddbf1e5b9cbefa19a14
Rodgers Ogada <ogadarodgers@gmail.com>
2025-04-15 09:15:01 +0300

    updated readme and included json data

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,7 @@ import './App.css';
 import TaskInputForm from './TaskInputForm';
 import TaskList from './TaskList';
 import Footer from './Footer';
-
-const myTasks = [
-  {
-    id: 1,
-    title: 'Task 1',
-    description: 'This is the first task.',
-    completed: false,
-  },
-  {
-    id: 2,
-    title: 'Task 2',
-    description: 'This is the second task.',
-    completed: true,
-  },
-  {
-    id: 3,
-    title: 'Task 3',
-    description: 'This is the third task.',
-    completed: false,
-  },
-  {
-    id: 4,
-    title: 'Task 4',
-    description: 'This is the fourth task.',
-    completed: true,
-  },
-  {
-    id: 5,
-    title: 'Task 5',
-    description: 'This is the fifth task.',
-    completed: false,
-  },
-];
+import { myTasks } from './tasks';
 
 function App() {
   const [tasks, setTasks] = useState(myTasks);
@@ -51,7 +19,7 @@ function App() {
       <TaskInputForm addTask={addTask} />
       <h2>Task List</h2>
       <TaskList tasks={tasks} />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
---
+++ src/tasks.js
@@ -0,0 +1,32 @@
+export const myTasks = [
+  {
+    id: 1,
+    title: 'Task 1',
+    description: 'This is the first task.',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Task 2',
+    description: 'This is the second task.',
+    completed: true,
+  },
+  {
+    id: 3,
+    title: 'Task 3',
+    description: 'This is the third task.',
+    completed: false,
+  },
+  {
+    id: 4,
+    title: 'Task 4',
+    description: 'This is the fourth task.',
+    completed: true,
+  },
+  {
+    id: 5,
+    title: 'Task 5',
+    description: 'This is the fifth task.',
+    completed: false,
+  },
+];
