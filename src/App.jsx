d351fad0561cfddc0a714b0c9e15fa2c7f3a1c78
Rodgers Ogada <ogadarodgers@gmail.com>
2025-04-15 10:03:19 +0300

    logic for fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import TaskInputForm from './TaskInputForm';
 import TaskList from './TaskList';
@@ -6,8 +6,30 @@ import Footer from './Footer';
 import { myTasks } from './tasks';
 
 function App() {
-  const [tasks, setTasks] = useState(myTasks);
-  console.log(tasks);
+  const apiUrl = 'http://localhost:3000/tasks';
+  const [tasks, setTasks] = useState([]);
+  // console.log(tasks);
+
+  console.log('trial 1');
+
+  useEffect(() => {
+    // logic here for side effects
+    // like fetching data from an API or local storage
+    // console.log('Component mounted or updated');
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        setTasks(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []); // dependency array
+
+  console.log('trial');
+  console.log(tasks)
 
   const addTask = (newTask) => {
     setTasks((previousTasks) => [newTask, ...previousTasks]);
